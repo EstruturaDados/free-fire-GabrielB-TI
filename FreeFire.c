9e0f2bfb768dac6487bb97152808a197fa7d0d59
Gabriel B Lisboa <gabrielblisboa1800@gmail.com>
2025-10-22 22:49:35 +0000

    Adicionado Registro, Inspeçao e remoçao

--- FreeFire.c
+++ FreeFire.c
@@ -48,5 +48,57 @@ int main() {
                     printf("Item adicionado com sucesso!\n");
                 }
                 break;
+
+            case 2:
+                if (total == 0) {
+                    printf("Nenhum item para remover.\n");
+                } else {
+                    char nomeRemover[50];
+                    int encontrado = 0;
+                    printf("\nNome do item a remover: ");
+                    fgets(nomeRemover, 50, stdin);
+                    nomeRemover[strcspn(nomeRemover, "\n")] = 0;
+
+                    for (int i = 0; i < total; i++) {
+                        if (strcmp(mochila[i].nome, nomeRemover) == 0) {
+                            for (int j = i; j < total - 1; j++) {
+                                mochila[j] = mochila[j + 1];
+                            }
+                            total--;
+                            encontrado = 1;
+                            printf("Item removido com sucesso!\n");
+                            break;
+                        }
+                    }
+
+                    if (!encontrado) {
+                        printf("Item não encontrado.\n");
+                    }
+                }
+                break;
+
+            case 3:
+                if (total == 0) {
+                    printf("Nenhum item cadastrado.\n");
+                } else {
+                    printf("\n--- Itens Cadastrados ---\n");
+                    printf("%-20s %-20s %-10s\n", "NOME", "TIPO", "QUANTIDADE");
+                    printf("-----------------------------------------------\n");
+                    for (int i = 0; i < total; i++) {
+                        printf("%-20s %-20s %-10d\n",
+                               mochila[i].nome, mochila[i].tipo, mochila[i].quantidade);
+                    }
+                }
+                break;
+
+            case 0:
+                printf("Saindo do programa...\n");
+                break;
+
+            default:
+                printf("Opção inválida!\n");
+        }
+    } while (opcao != 0);
+
     return 0;
 }
