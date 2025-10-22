0464e30ff560b3b88f34faa2d93f2276cf7c843a
Gabriel B Lisboa <gabrielblisboa1800@gmail.com>
2025-10-22 22:42:42 +0000

    Adicionado registro de itens na mochila

--- FreeFire.c
+++ FreeFire.c
@@ -16,8 +16,7 @@ int main() {
     int total = 0;
     int opcao;
 
-do {
-
+    do {
         printf("\n=== INVENTÁRIO BÁSICO ===\n");
         printf("1. Adicionar item\n");
         printf("2. Remover item\n");
@@ -25,8 +24,29 @@ do {
         printf("0. Sair\n");
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
-        getchar();
+        getchar(); // Limpar o buffer do teclado
 
+        switch (opcao) {
+            case 1:
+                if (total >= MAX_ITENS) {
+                    printf("Mochila cheia! Não é possível adicionar mais itens.\n");
+                } else {
+                    printf("\n--- Adicionar Item ---\n");
+                    printf("Nome: ");
+                    fgets(mochila[total].nome, 50, stdin);
+                    mochila[total].nome[strcspn(mochila[total].nome, "\n")] = 0;
+
+                    printf("Tipo: ");
+                    fgets(mochila[total].tipo, 30, stdin);
+                    mochila[total].tipo[strcspn(mochila[total].tipo, "\n")] = 0;
+
+                    printf("Quantidade: ");
+                    scanf("%d", &mochila[total].quantidade);
+                    getchar();
+
+                    total++;
+                    printf("Item adicionado com sucesso!\n");
+                }
+                break;
     return 0;
-
-    }
+}
